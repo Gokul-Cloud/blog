820822ad0529b7e7692cd97f2ea41636486d17e9
gokul-cloud <gokulgiritv@gmail.com>
2021-04-12 17:53:26 +0530

    first

---
+++ blog/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from './Components/header'
+import DisplayAllPosts from './Components/DisplayAllPosts'
+const App = ( ) => {
+  return (
+    <div>
+      <Header/>
+      <DisplayAllPosts />
+    </div>
+  );
+};
+export default App;
---
+++ blog/src/Components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+const header = () => {
+    return (
+        <div>
+            <h1 className="header">Simple Blog</h1>
+        </div>
+    )
+}
+
+export default header
